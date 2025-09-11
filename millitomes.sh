5ddc6d321c4776a3550afc4e29e1cc34b31ace38
Bruce W. Herr II <bherr@iu.edu>
2025-09-11 13:31:26 -0400

    Update millitomes list to add pointcloud view Fixes #73

--- millitomes.sh
+++ millitomes.sh
@@ -6,6 +6,7 @@ echo
 for f in `find output-data -name index.html | sort`; do
   name=$(echo $f | cut -d '/' -f 3)
   version=$(echo $f | cut -d '/' -f 4)
-  glb=$(ls `dirname $f`/*.glb)
-  echo " * $name $version ([EUI](https://cns-iu.github.io/hra-amap/$f)) ([3d view](https://sandbox.babylonjs.com/?assetUrl=https://cns-iu.github.io/hra-amap/$glb))"
+  glb=$(ls `dirname $f`/*-*.glb)
+  fitGlb=$(ls `dirname $f`/point_cloud_transformation_fit.glb)
+  echo " * $name $version ([EUI](https://cns-iu.github.io/hra-amap/$f)) ([3d view](https://sandbox.babylonjs.com/?assetUrl=https://cns-iu.github.io/hra-amap/$glb)) ([pointcloud view](https://sandbox.babylonjs.com/?assetUrl=https://cns-iu.github.io/hra-amap/$fitGlb)) "
 done
